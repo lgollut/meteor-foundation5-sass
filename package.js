0d2b44a787779369ef856a64f6560191680d073d
Louis Gollut <louis.gollut@gmail.com>
2014-02-14 16:16:28 +0100

    Update package.js
    To add scss usage

--- package.js
+++ package.js
@@ -3,6 +3,8 @@ Package.describe({
 });
 
 Package.on_use(function (api) {
+  api.use('scss', 'client');
+  
   api.add_files([
     "js/foundation/foundation.js",
     "js/foundation/foundation.abide.js",
